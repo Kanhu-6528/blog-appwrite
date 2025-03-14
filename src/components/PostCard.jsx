14bc9a3adb65b396218594f29fff9e2a07081a81
Kanhu-6528 <kanhuc74@gmail.com>
2025-03-14 14:18:12 +0530

    Build components

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import service from "../appwrite/conf";
+import { Link } from "react-router-dom";
+
+const PostCard = ({ $Id, title, featuredImage }) => {
+  return (
+    <Link to={`/post/${$Id}`}>
+      <div className="w-full bg-gray-100 rounded-xl p-4">
+        <div className="w-full justify-center mb-4">
+          <img src={service.getFilePreview(featuredImage)} alt={title} />
+        </div>
+        <h2>{title}</h2>
+      </div>
+    </Link>
+  );
+};
+
+export default PostCard;
