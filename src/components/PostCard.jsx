fddee7021ddb9dbc65ea74b6b742b62d14c3b113
Kanhu-6528 <kanhuc74@gmail.com>
2025-03-14 16:05:24 +0530

    Build auth components

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -7,9 +7,13 @@ const PostCard = ({ $Id, title, featuredImage }) => {
     <Link to={`/post/${$Id}`}>
       <div className="w-full bg-gray-100 rounded-xl p-4">
         <div className="w-full justify-center mb-4">
-          <img src={service.getFilePreview(featuredImage)} alt={title} />
+          <img
+            src={service.getFilePreview(featuredImage)}
+            alt={title}
+            className="rounded-xl"
+          />
         </div>
-        <h2>{title}</h2>
+        <h2 className="text-xl font-bold">{title}</h2>
       </div>
     </Link>
   );
